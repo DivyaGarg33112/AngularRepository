0e9297053b5a24146f0b84ac95b2ec138474362c
divyagarg <java.divya.garg@gmail.com>
2019-12-01 11:59:22 +0530

    FirstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SecondApp';
+  employeeFirstName:string='Divya';
+  employeeLastName:string='Garg';
+  num1=10;
+  num2=20;
+  isDisabled=false;
+  imageUrl="https://pluralsight2.imgix.net/paths/images/angular-14a0f6532f.png";
+  userName='Divya Garg';
+
+  changeMyTitle(){
+    this.title="Data Binding Demo";    
+  }
+  changeUserName(uname:string){
+    this.userName=uname;
+  }
 }
